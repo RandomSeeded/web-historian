db7e4b4285bfb583b9aaed05e758c6f12b7a37fa
RandomSeeded <nateawillard@gmail.com>
2015-09-10 14:55:54 -0700

    Implemented cron worker

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,6 +1,9 @@
 var http = require("http");
 var handler = require("./request-handler");
 var initialize = require("./initialize.js");
+var fetcher = require("../workers/htmlfetcher");
+var cron = require('cron');
+
 
 // Why do you think we have this here?
 // HINT: It has to do with what's in .gitignore
@@ -16,4 +19,16 @@ if (module.parent) {
   server.listen(port, ip);
   console.log("Listening on http://" + ip + ":" + port);
 }
+
+
+
+//CRON THAT STUFF
+var CronJob = cron.CronJob;
+var fetchPages = new CronJob({
+  cronTime: '00 * * * * *',
+  onTick: fetcher,
+  start: true,
+  timeZone: "America/Los_Angeles"
+});
+
 
